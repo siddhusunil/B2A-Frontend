45622f050acf7ca020cbde05ab3749cbadd79f82
siddhusunil <142740062+siddhusunil@users.noreply.github.com>
2024-12-16 17:29:02 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import { UserContextProvider } from './context/UserContext.jsx';
 import { CourseContextProvider } from './context/CourseContext.jsx';
 
-export const server = "http://localhost:8000";
+export const server = "https://b2a.onrender.com";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
